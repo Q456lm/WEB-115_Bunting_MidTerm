af8df566aa6a880a4200b3781b40e307d49b5082
Q456lm <cbbunting1@my.waketech.edu>
2025-04-03 12:09:39 -0400

    I did my best, I have no regrets

--- codingExam.js
+++ codingExam.js
@@ -11,7 +11,13 @@ function startcalc(){
     }
     int_rate = 0.0575
     month_payment = (((int_rate/12)*loan)/(1-Math.pow(1+(int_rate/12), (loan_years*-12)))).toFixed(2);
-    console.log(month_payment)
+    console.log(month_payment);
+    int_payed = (month_payment * loan_years*12)-loan;
+    loan_cost = loan + theywantedmetosubtractitfromthewhole;
+    document.getElementById("loan_years").innerHTML = `${loan_years} years`
+    document.getElementById("an_rate").innerHTML = `Annual Interest Rate: 5.75%`
+    document.getElementById("an_rate").innerHTML = `Monthly Interest Rate: ${(int_rate/12)*100}%`
+    document.getElementById("an_rate").innerHTML = `${theywantedmetosubtractitfromthewhole}`
 }
 
 but.addEventListener("click",startcalc()); //runs start calc function at click of button
